69e6d6a6580ac05c48485ca748a3d540f518594e
Dindin4987 <144334821+Dindin4987@users.noreply.github.com>
2024-09-29 20:36:01 +0800

    update

--- src/components/EditForm.jsx
+++ src/components/EditForm.jsx
@@ -31,36 +31,66 @@ function EditForm({ type, transaction, onClose }) {
   };
 
   return (
-    <div className="modal">
-      <form onSubmit={handleSubmit}>
-        <h3>Edit {type === "expense" ? "Expense" : "Income"}</h3>
-        <input
-          type="text"
-          value={category}
-          onChange={(e) => setCategory(e.target.value)}
-          placeholder="Category"
-        />
-        <input
-          type="number"
-          value={amount}
-          onChange={(e) => setAmount(e.target.value)}
-          placeholder="Amount"
-        />
-        <input
-          type="text"
-          value={comment}
-          onChange={(e) => setComment(e.target.value)}
-          placeholder="Comment"
-        />
-        <input
-          type="date"
-          value={date}
-          onChange={(e) => setDate(e.target.value)}
-        />
-        <button type="submit">Save</button>
-        <button type="button" onClick={onClose}>
-          Cancel
-        </button>
+    <div className="bg-gray-700 p-6 rounded-lg w-1/3 modal">
+      <form onSubmit={handleSubmit} className="text-black">
+        <h3 className="text-2xl mb-7">
+          Edit {type === "expense" ? "Expense" : "Income"}
+        </h3>
+
+        <div className="mb-4">
+          <label className="block text-gray-300">Category</label>
+          <input
+            type="text"
+            value={category}
+            onChange={(e) => setCategory(e.target.value)}
+            placeholder="Category"
+          />
+        </div>
+
+        <div className="mb-4">
+          <label className="block text-gray-300">Sum (UAH)</label>
+          <input
+            type="number"
+            value={amount}
+            onChange={(e) => setAmount(e.target.value)}
+            placeholder="Amount"
+          />
+        </div>
+
+        <div className="mb-4">
+          <label className="block text-gray-300">Comment</label>
+          <textarea
+            type="text"
+            value={comment}
+            onChange={(e) => setComment(e.target.value)}
+            placeholder="Comment"
+          />
+        </div>
+
+        <div className="mb-4 mr-7">
+          <label className="block text-gray-300">Date</label>
+          <input
+            type="date"
+            value={date}
+            onChange={(e) => setDate(e.target.value)}
+          />
+        </div>
+
+        <div className="flex">
+          <button
+            type="submit"
+            className="bg-green-500 text-white py-2 px-4 rounded-full w-28 hover:bg-green-600 mr-5"
+          >
+            Save
+          </button>
+          <button
+            type="button"
+            onClick={onClose}
+            className="bg-gray-500 text-white py-2 px-4 rounded-full w-28 hover:bg-green-600"
+          >
+            Cancel
+          </button>
+        </div>
       </form>
     </div>
   );
